ff668cc537bacee05f3e1651c65264f639f098f3
HananBahy <hananbahy12@gmail.com>
2018-03-17 17:05:27 +0200

    simple example

---
+++ sort linked list.py
@@ -0,0 +1,17 @@
+from linkedlistuno import linkedlistuno
+
+mylist=linkedlistuno()
+mylist.add(100)
+mylist.add(500)
+mylist.add(70)
+mylist.add(1)
+mylist.add(-1)
+mylist.add(8)
+mylist.add(40)
+mylist.add(70)
+mylist.add(5)
+mylist.add(-1)
+mylist.print_linked2()
+mylist.selectionsort()
+print("after sorting:")
+mylist.print_linked2()
